7d95efcb7d120d16725d3c5cded97b0f24af2d2b
Alexa Cristina <ac@mysite.org>
2020-06-22 22:34:54 +0300

    More info

--- src/Language.js
+++ src/Language.js
@@ -1,7 +1,7 @@
 export const infoHtml = (language) => {
     return language === 'ro'
-    ? '<strong>Relația cazurilor confirmate</strong>.<br/>Date de pe covid19.geo-spatial.org<br/>Situaţia până la data în care s-au raportat oficial aceste informaţii.<br/>Dați click în afara cercului pentru a deselecta.'
-    : '<strong>Relationship between confirmed cases</strong>.<br/>Data from covid19.geo-spatial.org<br/>The status until the date this information has been officially reported.<br/>Click outside the circle to clear the selection.';
+    ? '<strong>Relația cazurilor confirmate. Fiecare punct reprezintă un om.</strong><br/><br/>Date de pe covid19.geo-spatial.org<br/>Situaţia până la data în care s-au raportat oficial aceste informaţii.<br/>Dați click în afara cercului pentru a deselecta.'
+    : '<strong>Relationship between confirmed cases. Each point represents a person.</strong><br/><br/>Data from covid19.geo-spatial.org<br/>The status until the date this information has been officially reported.<br/>Click outside the circle to clear the selection.';
 }
 
 export const status = (language) => {
