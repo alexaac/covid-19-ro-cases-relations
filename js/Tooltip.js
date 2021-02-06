7a08afb9296704e22b3f956c4991910b8fdceb18
Alexa Cristina <52584675+alexaac@users.noreply.github.com>
2021-02-06 23:57:29 +0200

    Center info div

--- js/Tooltip.js
+++ js/Tooltip.js
@@ -165,8 +165,8 @@ export const toggleInfo = (infoStatus, language) => {
             .style('max-width', '100%')
             .style('opacity', .9);
         tooltip_div.html(Language.infoHtml(language))
-            .style('left', Config.width / 2 + 'px')
-            .style('top', Config.height / 2 + 'px')
+            .style('margin-left', auto)
+            .style('margin-right', auto)
             .style('display', null);
         infoStatus = false;
     } else {
